6cec8e0393c0d1af6fae6dc96a97627f153c7103
諒太 林 <ryouturn@gmail.com>
2024-05-25 09:30:01 +0900

    index を実装やりかけ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,55 +7,26 @@ import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 import React from "react";
 import { Inter } from "next/font/google";
 import ShowArticles from "./components/showArticles";
-import { grey } from "@mui/material/colors";
-import { dark } from "@mui/material/styles/createPalette";
+import getDesignTokens from './setting';
+import ShowReadMe from "./components/README";
 
 type MenuList = {
+  index:Number;
   name:string;
-  link:string;
+  link:React.ReactNode;
   icon:React.ReactNode;
 }
 const menulist: MenuList[] = [
-  {name:"Home",link:'./home',icon:<HomeIcon/>},
-  {name:"ReadMe",link:'../README',icon:<ArticleIcon/>},
-  {name:"Author",link:'https://sites.google.com/view/juppiter',icon:<PersonIcon/>},
-  {name:"Contact",link:'./contact',icon:<ContactSupportIcon/>}
+  {index:0, name:"Home",link:<ShowArticles/>, icon:<HomeIcon/>},
+  {index:1, name:"ReadMe",link:<ShowReadMe/>, icon:<ArticleIcon/>},
+  {index:2, name:"Author",link:<a />,icon:<PersonIcon/>},
+  {index:3, name:"Contact",link:'./contact',icon:<ContactSupportIcon/>}
 ];
 
-const getDesignTokens = (mode: PaletteMode) => ({
-  palette: {
-    mode,
-    ...(mode === 'light'
-      ? {
-          // palette values for light mode
-          primary: {
-            main: '#440005',
-            dark: '#440005',
-          },
-          secondary: {
-            main: '#00443f',
-            dark: '#00443f',
-          },
-        }
-      : {
-          // palette values for dark mode
-          primary: {
-            main: '#440005',
-            dark: '#440005',
-          },
-          secondary: {
-            main: '#00443f',
-            dark: '#00443f',
-          },
-          background: {
-            default: '#1e1e1e',
-          },
-        }),
-  },
-});
+
 
 const inter = Inter({subsets: ['cyrillic']})
-const DrawerIsOpen :boolean = true;
+var nowMainField = <ShowArticles/>;
 
 export default function Home() {
   const [mode, setMode] = React.useState<PaletteMode>('dark');
@@ -73,6 +44,10 @@ export default function Home() {
 
   const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
+  const MenuOnClick = (index: number) => {
+    nowMainField = menulist[index].link;
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -86,9 +61,9 @@ export default function Home() {
         <Grid container spacing={5}>
           <Grid item xs="auto">
             <List>
-              {menulist.map(({name,link,icon}:MenuList) => (
+              {menulist.map(({index,name,icon}:MenuList) => (
                 <ListItem key={name} disablePadding>
-                  <ListItemButton>
+                  <ListItemButton onClick={() => MenuOnClick(index)}>
                     <ListItemIcon>
                       {icon}
                     </ListItemIcon>
@@ -99,7 +74,7 @@ export default function Home() {
             </List>
           </Grid>
           <Grid item xs={10} alignItems="center">
-            <ShowArticles></ShowArticles>
+            {nowMainField}
           </Grid>
         </Grid>
       </body>
